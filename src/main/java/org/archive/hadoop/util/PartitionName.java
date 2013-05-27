95f54a3615974f584d6002975e4eef95b2ed8386
Aaron Binns <aaron@archive.org>
2013-05-27 06:45:34 +0000

    Removed System.err debug statements.

--- src/main/java/org/archive/hadoop/util/PartitionName.java
+++ src/main/java/org/archive/hadoop/util/PartitionName.java
@@ -10,16 +10,12 @@ public class PartitionName {
 	
 	public static void setPartitionOutputName(Configuration conf, int partition, String name) {
 		String cName = getPartitionConfigName(partition);
-		System.err.format("Setting output name for partition(%d) (%s): %s\n", 
-				partition,cName,name);
 		conf.set(cName, name);
 	}
 
 	public static String getPartitionOutputName(Configuration conf, 
 			int partition) {
 		String cName = getPartitionConfigName(partition);
-		System.err.format("GOT output name for partition(%d) (%s): %s\n", 
-				partition,cName,conf.get(cName));
 		return conf.get(cName);
 	}
 }
